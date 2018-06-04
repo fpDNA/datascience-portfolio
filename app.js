d7381c8eadf94f2d39d2d528f9b3eda7798268c0
Felcy Selwyn <felcy@uw.edu>
2018-06-03 22:44:55 -0700

    fixed interaction-name

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//Part -1
 //Get user input 
 //<input id="name-input">
 
@@ -13,4 +15,22 @@ var name_out = document.getElementById("name-output");
 name_in.addEventListener("input", function () {
     name_out.innerText = name_in.value;
 });
+/*
+//Part-2 Add Animate button, get value of the currently selected option from the 
+//<select id="animation-select">
+
+
 var animate = document.getElementById("welcome");
+var click_animate = document.getElementById("animate");
+
+
+//Add one-time event listener for the animation
+click_animate.addEventListener("click", function () {
+    var style_add = document.getElementById("animation-select").value;
+    animate.classList.add(style_add);
+    //Add event listener for animationend
+    animate.addEventListener("animationend", function () {
+        animate.classList.remove(style_add);
+    }, { once: true });
+});
+*/
